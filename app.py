55709e2354f5dde72e11296b46d86a92ba9a6028
simkaramchandani01 <164099500+simkaramchandani01@users.noreply.github.com>
2025-03-09 20:36:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ USER_NAME = "simrank"
 VERSION = "1.0.0"
 
 def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
+    # Ensure the directory exists
+    if not os.path.exists(file_directory):
+        os.makedirs(file_directory)
+
     # Generate timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
 
     # Define filename with timestamp
     file_name = f"outputfile_{USER_NAME}_{VERSION}_timestamp_{timestamp}.txt"
-    file_path = os.join(file_directory, file_name)
+    file_path = os.path.join(file_directory, file_name)
 
     # Write the timestamp inside the file
     with open(file_path, "w") as file:
@@ -24,3 +28,4 @@ def add_numbers(a:int, b:int) -> int:
 
 if __name__ == "__main__":
     say_hi()
+
